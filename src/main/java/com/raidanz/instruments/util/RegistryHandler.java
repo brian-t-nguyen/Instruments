62d21efb19bac0d9e89f6874524630e74aaac25c
raidanz <xbriann1@gmail.com>
2020-03-26 18:00:27 -0400

    Initial commit

---
+++ src/main/java/com/raidanz/instruments/util/RegistryHandler.java
@@ -0,0 +1,19 @@
+package com.raidanz.instruments.util;
+
+import com.raidanz.instruments.Instruments;
+import com.raidanz.instruments.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, Instruments.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+    // Items
+    public static final RegistryObject<Item> GUITAR = ITEMS.register("guitar", ItemBase::new);
+}
